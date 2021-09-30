f2efd8a155a9c9186d30271ef2e061dc7f141181
Đặng Vũ Phương Uyên <90638068+dvphuonguyen@users.noreply.github.com>
2021-09-30 13:28:08 +0700

    Update BTVN_BAI6.cs

--- Buổi 1/BTVN_BAI6.cs
+++ Buổi 1/BTVN_BAI6.cs
@@ -16,49 +16,5 @@ namespace BTVN_BAI6
             Date date = new Date(ngay, thang, nam);
             Console.WriteLine( "\t==>Ngày {0}/{1}/{2} là {3} ", ngay, thang, nam, date.NgayTrongTuan());
         }
-        /*
-        static int[] month_table = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        static int[] leap_month_table = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        static string[] day = { "Thu tu", "Thu nam", "Thu sau", "Thu 7", "Chu nhat", "Thu hai", "Thu ba" };
-
-        public static bool isLeapYear(int year)
-        {
-            return (((year % 4 == 0) && (year % 100 != 0)) ||
-                    (year % 400 == 0));
-        }
-
-        public static void GetDayOfWeek(int d, int m, int y)
-        {
-            int TongNgay = 0;
-            for (int i = 1970; i < y; i++)
-            {
-                if (isLeapYear(i))
-                    TongNgay += 366;
-                else
-                    TongNgay += 365;
-            }
-            Console.Write(TongNgay);
-            for (int i = 1; i < m; i++)
-                if (isLeapYear(y))
-                    TongNgay += leap_month_table[i];
-                else
-                    TongNgay += month_table[i];
-            TongNgay += d;
-            Console.Write(TongNgay);
-            Console.Write(day[TongNgay % 7]);
-
-        }
-        static void Main(string[] args)
-        {
-            Console.Write("Nhap Ngay: ");
-            int ngay = int.Parse(Console.ReadLine());
-            Console.Write("Nhap thang: ");
-            int thang = int.Parse(Console.ReadLine());
-            Console.Write("Nhap nam: ");
-            int nam = int.Parse(Console.ReadLine());
-
-            GetDayOfWeek(ngay, thang, nam);
-        }
-        */
     }
 }
