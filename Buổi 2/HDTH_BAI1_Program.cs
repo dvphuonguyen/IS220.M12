d59fe5e0bd50facf85b1f6c900e131616760c026
Đặng Vũ Phương Uyên <90638068+dvphuonguyen@users.noreply.github.com>
2021-10-13 16:38:56 +0700

    Add files via upload

---
+++ Buổi 2/HDTH_BAI1_Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HDTH_BAI1
+{
+    class HDTH_BAI1_Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap thong tin tai khoan:");
+            Console.Write("\tNhap thong tin so tai khoan:");
+            long soTK = long.Parse(Console.ReadLine());
+            Console.Write("\tNhap thong tin ten tai khoan:");
+            string tenTK = Console.ReadLine();
+            Console.Write("\tNhap thong tin so tien:");
+            double soTien = double.Parse(Console.ReadLine());
+            HDTH_BAI1_TaiKhoan taiKhoan = new HDTH_BAI1_TaiKhoan(soTK, tenTK, soTien);
+            Console.WriteLine(taiKhoan);
+            Console.Write("Nhap so tien can nap:");
+            double tienNap = double.Parse(Console.ReadLine());
+            if (taiKhoan.NapTien(tienNap))
+            {
+                Console.WriteLine("Nap tien thanh cong");
+            }
+            else
+            {
+                Console.WriteLine("Nap tien that bai");
+            }
+            if (taiKhoan.RutTien())
+            {
+                Console.WriteLine("Rut tien thanh cong");
+            }
+            else
+            {
+                Console.WriteLine("Rut tien that bai");
+            }
+        }
+    }
+}
---
+++ Buổi 2/HDTH_BAI1_TaiKhoan.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDTH_BAI1
+{
+    class HDTH_BAI1_TaiKhoan
+    {
+        private long soTK;
+        private string tenTK;
+        private double soTien;
+        public long SoTK
+        {
+            get => soTK;
+            set
+            {
+                soTK = value;
+            }
+        }
+        public string TenTK
+        {
+            get => tenTK;
+            set
+            {
+                tenTK = value;
+            }
+        }
+        public double SoTien
+        {
+            get => soTien;
+            set
+            {
+                soTien = value;
+            }
+        }
+        public HDTH_BAI1_TaiKhoan()
+        {
+            soTK = 0;
+            tenTK = null;
+            soTien = 0;
+        }
+        public HDTH_BAI1_TaiKhoan(long tk, string ten, double tien)
+        {
+            soTK = tk;
+            tenTK = ten;
+            soTien = tien;
+        }
+        
+
+        public override string ToString()
+        {
+            return string.Format("So tai khoan: {0},Tên: {1}, So tien: {2} USD", soTK, tenTK, soTien);
+        }
+
+        public bool NapTien(double tienNap)
+        {
+            if (tienNap >= 0)
+            {
+                soTien += tienNap;
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool RutTien()
+        {
+            Console.Write("Nhap so tien can rut:");
+            double tienRut = double.Parse(Console.ReadLine());
+            if (tienRut > 0 && (soTien - tienRut - tienRut * 0.01) >= 0)
+            {
+                soTien = soTien - (1.01 * tienRut);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
